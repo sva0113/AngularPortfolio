93a82942391b4b1b9d833bf3afbfba2ff90f1bef
sva0113 <siva,jakka0113@gmail.com>
2018-07-03 15:10:19 -0700

    Initial commit for added header

---
+++ port-folio-cg-angular/app/about/about.js
@@ -0,0 +1,4 @@
+angular.module('y').controller('AboutCtrl',function($scope){
+
+
+});
---
+++ port-folio-cg-angular/app/skills/Skills-spec.js
@@ -0,0 +1,18 @@
+describe('SkillsCtrl', function() {
+
+    beforeEach(module('y'));
+
+    var scope,ctrl;
+
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('SkillsCtrl', {$scope: scope});
+    }));
+
+    it('should ...', inject(function() {
+
+        expect(1).toEqual(1);
+        
+    }));
+
+});
